8d3f7de6ab1741d767161d494da61bb4ed465f2b
Alatise Oluwaseun <holynation667@gmail.com>
2023-03-23 18:04:54 +0100

    fix: Adjusted the code

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,6 +2,8 @@
 """
 Define class SessionDButh
 """
+from flask import request
+from datetime import datetime, timedelta
 from .session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
