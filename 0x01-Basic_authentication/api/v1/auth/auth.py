5a346440bd01b24cc4a5eed05dfc701c1dc33b6a
Alatise Oluwaseun <holynation667@gmail.com>
2023-03-23 18:12:28 +0100

    fix: Correcte the code

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,15 +10,16 @@ class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """requires authentication"""
-        if path is None or excluded_paths is None or not excluded_paths:
-            return True
-        if path in excluded_paths or path + "/" in excluded_paths:
-            return False
-
-        pathWildCard = "stat"
-        for e_path in excluded_paths:
-            if e_path.endswith('*'):
-                if path.startswith(pathWildCard):
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
                     return False
         return True
 
